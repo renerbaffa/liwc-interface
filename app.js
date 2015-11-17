91bee3d7c7e9ec0a526d34ae41552488d89eb7a4
Rener Baffa da Silva <renerbaffa@gmail.com>
2015-11-17 18:22:44 -0200

    Creating the connection with mysql db

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var app = express();
+var mysql = require('mysql');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,13 +22,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -58,3 +56,9 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root"
+});
